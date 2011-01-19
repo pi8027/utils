3af3155d42854c12ef7f700c41dfa3cd5fc49eb1
kazuhiko Sakaguchi <pi8027@gmail.com>
2011-01-19 23:16:43 +0900

    rewrite tmux_battery.hs

--- tmux_battery.hs
+++ tmux_battery.hs
@@ -2,26 +2,34 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as BS
-import Data.Attoparsec hiding (satisfy)
 import Data.Attoparsec.Char8
 import Control.Applicative
+import Control.Monad
 import System.Process
 
-acpiParser :: Parser (Int, Int, Int, Int)
-acpiParser = do
-    p <- string (BS.pack "Battery ") >> decimal >>
-        string (BS.pack ": Discharging, ") >> decimal
-    t1 <- string (BS.pack "%, ") >> decimal
-    t2 <- satisfy (':' ==) >> decimal
-    t3 <- satisfy (':' ==) >> decimal
-    string (BS.pack " remaining\n")
-    return (p, t1, t2, t3)
+data BatteryState =
+    BatCharging Int Int Int Int | BatDischarging Int Int Int Int | BatFull Int
+
+acpiParser :: Parser BatteryState
+acpiParser = string (BS.pack "Battery ") >>
+    decimal >> fullP <|> chargingP <|> dischargingP where
+    fullP = string (BS.pack ": Full, ") *>
+        liftM BatFull decimal <* string (BS.pack "%\n")
+    chargingP = string (BS.pack ": Charging, ") *>
+        moreStateP BatCharging <* string (BS.pack " until charged\n")
+    dischargingP = string (BS.pack ": Discharging, ") *>
+        moreStateP BatDischarging <* string (BS.pack " remaining\n")
+    moreStateP f = liftM4 f
+        decimal (string (BS.pack "%, ") >> decimal)
+        (satisfy (':' ==) >> decimal) (satisfy (':' ==) >> decimal)
 
 main :: IO ()
-main = do
-    acpiResult <- BS.pack <$> readProcess "acpi" [] ""
-    case parse acpiParser acpiResult of
-        Done _ (p, t1, t2, t3) -> BS.putStr $ BS.pack $
-            "(bat: " ++ show p ++ "%, " ++ show t1 ++ ":" ++ show t2 ++ ") "
-        _ -> return ()
+main = readProcess "acpi" [] "" >>=
+    putStr . strResult . parse acpiParser . BS.pack where
+    strResult (Done _ (BatCharging p t1 t2 t3)) =
+        "(bat+: " ++ show p ++ "%, " ++ show t1 ++ ":" ++ show t2 ++ ") "
+    strResult (Done _ (BatDischarging p t1 t2 t3)) =
+        "(bat-: " ++ show p ++ "%, " ++ show t1 ++ ":" ++ show t2 ++ ") "
+    strResult (Done _ (BatFull n)) = "(bat: full)"
+    strResult _ = ""
 
